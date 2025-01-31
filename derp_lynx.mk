533b380a18dde3d55cb79069695205ba147a884e
Yaznas <sanjshrestha53@gmail.com>
2025-01-31 16:13:21 +0545

    lynx: Derp it

--- derp_lynx.mk
+++ derp_lynx.mk
@@ -4,22 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common stuff
+# Inherit some common DerpFest stuff
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configuration
 DEVICE_CODENAME := lynx
 DEVICE_PATH := device/google/lynx
 VENDOR_PATH := vendor/google/lynx
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
-$(call inherit-product, device/google/gs201/lineage_common.mk)
-$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
+$(call inherit-product, device/google/gs201/derp_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-derp.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7a
-PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+PRODUCT_NAME := derp_$(DEVICE_CODENAME)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -31,3 +31,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
+
+DERP_BUILDTYPE := Official
